a78265826a9bb47cdcdc987ae5df333e42959509
Denis Mory <denismr7@gmail.com>
2021-08-06 22:33:59 +0200

    Created image and container with .NET app

--- Startup.cs
+++ Startup.cs
@@ -68,9 +68,9 @@ namespace Shop
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shop v1"));
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
